15bb09155cdc8fcc982c573341ed097143950daa
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-05-19 13:49:50 +0300

    Countdown Timer
    Creating and implementing a countdown timer (simple at first, and more difficult in further commits).

---
+++ script.js
@@ -0,0 +1,23 @@
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const countdownElement = document.getElementById('countdown');
+
+/*
+I could have originally given a class to the html element
+and taken it through querySelector, but I'm practicing.
+Therefore, I chose a different approach, to "take" the values through the id attribute
+*/
+
+function updateCountdown() {
+	const minutes = Math.floor(time / 60);
+	let seconds = time % 60;
+
+	seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+	countdownElement.innerHTML = `${minutes} : ${seconds}`;
+
+	time--;
+}
+
+setInterval(updateCountdown, 1000);
