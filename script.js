9ee7f475b8c23cc3d92a41dcd87469c226e0f94f
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-05-22 12:25:43 +0300

    Countdown Timer
    Implementation of a countdown timer, which counts down the time from the current date to the moment of opening the "store" (the date I set). Completely reworked the counter from the last commit (an attempt to implement something that the customer requires, although very basic and simple).

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const startingMinutes = 10;
-let time = startingMinutes * 60;
+const countdown = () => {
 
-const countdownElement = document.getElementById('countdown');
+	const countDate = new Date('December, 4, 2022 00:00:00').getTime();
+	const now = new Date().getTime();
+	const gap = countDate - now;
 
-/*
-I could have originally given a class to the html element
-and taken it through querySelector, but I'm practicing.
-Therefore, I chose a different approach, to "take" the values through the id attribute
-*/
+	const second = 1000;
+	const minute = second * 60;
+	const hour = minute * 60;
+	const day = hour * 24;
 
-function updateCountdown() {
-	const minutes = Math.floor(time / 60);
-	let seconds = time % 60;
+	let openingDay = Math.floor(gap / day);
+	let openingHour = Math.floor((gap % day) / hour);
+	let openingMinute = Math.floor((gap % hour) / minute);
+	let openingSecond = Math.floor((gap % minute) / second);
 
-	seconds = (seconds < 10) ? "0" + seconds : seconds;
+	document.querySelector('.day').innerText = openingDay;
+	document.querySelector('.hour').innerText = openingHour;
+	document.querySelector('.minute').innerText = openingMinute;
+	document.querySelector('.second').innerText = openingSecond;
 
-	countdownElement.innerHTML = `${minutes} : ${seconds}`;
-	if (time != 0) {
-		time--;
-	}
-}
+};
 
-setInterval(updateCountdown, 1000);
+setInterval(countdown, 1000);
